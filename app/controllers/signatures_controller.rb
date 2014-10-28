6e7785909fe23fd16575c123903058ccb67ddd02
Andrew Kuklewicz <andrew@beginsinwonder.com>
2014-10-28 13:11:12 -0400

    cleanup

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -1,8 +1,8 @@
 class SignaturesController < ApplicationController
 
   def show
-    encoded = AuthSign.sign_data(params["to_sign"])
-    render :text => encoded, :status => 200
+    encoded = AuthSign.sign_data(params[:to_sign])
+    render text: encoded, status: 200
   end
 
 end
