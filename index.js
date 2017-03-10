857a11a49709fbfa72d36f0f020046697d481ec4
gcampo88 <gigi.campo@gmail.com>
2017-03-10 15:13:55 -0500

    Update lambda function to reflect new Gateway proxying, and add a test

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ const crypto = require('crypto');
 // Access Key ID: AKIAJZ5C7KQPL34SQ63Q
 const key = process.env.ACCESS_KEY;
 
-exports.handler = function (event, context, callback) {
-    let base64encodedData, data, signature;
-    
-    base64encodedData = event.to_sign_encoded;
-    data = new Buffer(base64encodedData, 'base64').toString('binary');
-    
-    signature = crypto.createHmac('sha1', key).update(data).digest('base64');
-    
-    // Lambda always wants to return JSON, so returning a plain string doesn't
-    // work out very well. Wrapping the signature in an object allows it to be 
-    // mapped in the API Gateway method's Integration Response to a properly
-    // formatted (i.e. naked) string in the HTTP response.
-    let output = { signature: signature };
-
-    signature ? callback(null, output) : callback(new Error('Signing failed'));
+exports.handler = (event, context, callback) => {
+    try {
+        const toSign = event.queryStringParameters.to_sign;
+        if (!toSign) {
+            callback(null, {statusCode: 500, headers: {}, body: 'No signature!'});
+        } else {
+            const signature = crypto.createHmac('sha1', key).update(toSign).digest('base64');
+            callback(null, {statusCode: 200, headers: {}, body: signature});
+        }
+    } catch (e) {
+        callback(e);
+    }
 };
