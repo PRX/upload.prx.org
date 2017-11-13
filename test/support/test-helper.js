015befeaefa56140b6b042140f92cc4ef2ffb847
Chris Kalafarski <chris@farski.com>
2017-11-13 16:44:53 -0500

    log

--- test/support/test-helper.js
+++ test/support/test-helper.js
@@ -2,4 +2,6 @@ if (!process.env.ACCESS_KEY) {
     require('dotenv').config({path: `${__dirname}/../../config/test.env`});
 }
 
+console.log(`>>>>>> ${process.env.ACCESS_KEY}`)
+
 global.expect = require('chai').expect;
