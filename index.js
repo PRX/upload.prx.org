e74d9ab2829c286c32e1b7969464675e1fb86c1b
Chris Kalafarski <chris@farski.com>
2019-05-08 16:11:37 -0400

    Remove unnecessary `use strict`

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const crypto = require('crypto');
 
 // User ARN: arn:aws:iam::561178107736:user/prx-upload
--- test/signature-test.js
+++ test/signature-test.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const helper = require('./support/test-helper');
 const handler = require('../index').handler;
 
