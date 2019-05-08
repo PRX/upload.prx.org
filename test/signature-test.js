3721de7f15a9a244bc0ce3f6160b508838935135
Chris Kalafarski <chris@farski.com>
2019-05-08 15:38:44 -0400

    Add AWS v4 signing support

--- test/signature-test.js
+++ test/signature-test.js
@@ -4,11 +4,20 @@ const helper = require('./support/test-helper');
 const handler = require('../index').handler;
 
 describe('signature', () => {
-  it('returns correct signature', () => {
+  it('returns correct v2 signature', () => {
     let event = {queryStringParameters: {to_sign: 'test'}};
     let context = null;
     let callback = (err, response) => {
       expect(response.body).to.equal('TyhhPs0RA37JFn+0oWNdm25HgBc=');
+    };
+    handler(event, context, callback);
+  });
+
+  it('returns correct v4 signature', () => {
+    let event = {queryStringParameters: {to_sign: 'AWS4-HMAC-SHA256test'}};
+    let context = null;
+    let callback = (err, response) => {
+      expect(response.body).to.equal('0b2ae1ead8f4df51204256b12c44c24af249a62d0eb8dfdd3eceeea73aa528f2');
     };
     handler(event, context, callback);
   });
