de763c53d4746a50d1e2fd18634c14fca50ba02b
Chris Kalafarski <chris@farski.com>
2019-05-09 15:12:26 -0400

    Fix v4 test result

--- test/signature-test.js
+++ test/signature-test.js
@@ -19,7 +19,7 @@ describe('signature', () => {
     let event = {queryStringParameters: {to_sign: 'AWS4-HMAC-SHA256test'}};
     let context = null;
     let callback = (err, response) => {
-      expect(response.body).to.equal('ea72177ce838f1faf1e9629624c6b0b7e0e07294a8aa7cb37195a0ea782af572');
+      expect(response.body).to.equal('b4d7d82a0860eec70f549065e7052c7f19f58fa37dd0d4a74493497e4a678907');
     };
     handler(event, context, callback);
   });
